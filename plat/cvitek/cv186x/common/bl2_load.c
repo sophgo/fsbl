d37de7594cdbcd1ada26d59c76e2b11b583fd6dc
ke.yi <ke.yi@sophgo.com>
2025-05-21 11:12:24 +0800

    patch 6311da34,fix reboot fail by adding bisr reset in bl2
    Change-Id: I4e2c6302ed2465d16c2054a8fd2641a596519138

--- plat/cvitek/cv186x/common/bl2_load.c
+++ plat/cvitek/cv186x/common/bl2_load.c
@@ -695,6 +695,23 @@ void fip_src_check(void)
 	}
 }
 
+/**
+ * @brief Built-In Self-Repair RESET
+ *
+ */
+void reset_rtc_bisr(void)
+{
+	// NOTICE("%s\n", __func__);
+	// All the RAM on the chip is bisr in the rom code, which is reset here
+	// bisr_pdgroup_en0 1 2 3 reset 0
+	mmio_write_32(0x050250dc, 0x0);
+	mmio_write_32(0x050250e0, 0x0);
+	mmio_write_32(0x050250e4, 0x0);
+	mmio_write_32(0x050250e8, 0x0);
+	// bisr_repair_en reset 0
+	mmio_write_32(0x050250d8, 0x0);
+}
+
 int load_rest(void)
 {
 	int retry = 0;
@@ -731,6 +748,8 @@ retry_from_flash:
 		if (load_loader_2nd(retry, &loader_2nd_entry) < 0)
 			continue;
 
+		// reset rtc bisr which occurs in rom
+		reset_rtc_bisr();
 		//MCU reset shoule deassert after image loaded from nor flash
 		mmio_write_32(0x05025018, 0x1fffff);
 		break;
